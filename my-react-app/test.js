fba882707bc7a587358523ff49e7aeaccd298e99
practicalcodewithshiva <practicalcodewithshiva@gmail.com>
2025-10-18 10:43:13 +0530

    added diff stages in sdlc

--- my-react-app/test.js
+++ my-react-app/test.js
@@ -1,1 +1,23 @@
-test.js
+Project 
+
+3 stages 
+
+Development branch ---- developers.  code 100% Development code push 
+
+
+100% code work 
+
+
+testers
+code push to testing enviornmet 
+QA branch --------   Testers
+
+
+bugs ayiteh raise chestaru 
+
+
+
+final stage Production real users 
+
+
+Production branch ----   Real time application use chesevalakosam
