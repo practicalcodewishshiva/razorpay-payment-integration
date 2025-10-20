3e51f6404cd09402fb81c7d8b11533055b63d752
Jeevan-code7 <jk29102023@gmail.com>
2025-10-20 17:10:49 +0530

    worked on a sidebar

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Sidebar from './Components/Sidebar/Sidebar'
+import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <Sidebar/>
+      <Sidebar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-react-app/src/Components/Sidebar/Sidebar.jsx
+++ my-react-app/src/Components/Sidebar/Sidebar.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import { IoMenuOutline } from "react-icons/io5";
+import { useState } from "react";
+import { Button, Drawer, Radio, Space } from "antd";
 
 export default function Sidebar() {
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState("left");
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  const onChange = (e) => {
+    setPlacement(e.target.value);
+  };
   return (
-    <div>Sidebar</div>
-  )
+    <>
+      <Space>
+        <Radio.Group value={placement} onChange={onChange}></Radio.Group>
+        <Button type="#f759ab" onClick={showDrawer}>
+          <IoMenuOutline />
+        </Button>
+      </Space>
+      <Drawer
+        title="Basic Drawer"
+        placement={placement}
+        closable={false}
+        onClose={onClose}
+        open={open}
+        key={placement}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
+  );
 }
