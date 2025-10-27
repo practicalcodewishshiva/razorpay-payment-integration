0486a895c585bbcbd6dd5e9d82d0503411f7747c
practicalcodewithshiva <practicalcodewithshiva@gmail.com>
2025-10-27 20:53:43 +0530

    new chage

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 
 import Products from "./Component/Products/Products";
-import { Routes } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Login/Login";
